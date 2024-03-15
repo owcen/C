f71bffc49576a07e1305fb5796ff6cbc8528a833
owcen <czypionkaanna@gmail.com>
2024-03-15 21:25:47 +0100

    zad 4,5

--- Zadania_2/Zad_4.c
+++ Zadania_2/Zad_4.c
@@ -1,3 +1,50 @@
-//
-// Created by Owcen on 12.03.2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    char name[20];
+    float grade;
+} Student;
+
+void swap(Student *a, Student *b) {
+    Student temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sortStudents(Student *table, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (table[j].grade < table[j + 1].grade) {
+                swap(&table[j], &table[j + 1]);
+            }
+        }
+    }
+}
+int main(){
+    int n = 100;
+    Student student_list[n];
+    for (int i = 0; i < n; i++){
+        if (i % 7) {
+            sprintf(student_list[i].name, "Anna");
+        } else if (i % 5) {
+            sprintf(student_list[i].name, "Dorota");
+        } else if (i % 4) {
+            sprintf(student_list[i].name, "Marcin");
+        } else if (i % 3) {
+            sprintf(student_list[i].name, "Kuba");
+        } else {
+            sprintf(student_list[i].name, "Darek");
+        }
+        student_list[i].grade = rand() % 101;
+    }
+
+    sortStudents(student_list, n);
+
+    printf("Posortowana lista studentow:\n");
+    for (int i = 0; i < n; i++){
+        printf("Imie: %s, ocena: %.2f\n", student_list[i].name, student_list[i].grade);
+    }
+
+    return 0;
+}
--- Zadania_2/Zad_5.c
+++ Zadania_2/Zad_5.c
@@ -1,3 +1,120 @@
-//
-// Created by Owcen on 12.03.2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CONTACTS 100
+
+typedef struct{
+    char name[20];
+    char surname[50];
+    char phone[10];
+} Contact;
+
+void addContact(Contact *contacts, int *contactsNumber, char *name, char *surname, char *phone){
+    if (*contactsNumber >= MAX_CONTACTS){
+        printf("Ksiazka pelna, nie mozna dodac nowego kontaktu.\n");
+        return;
+    } else {
+        strcpy(contacts[*contactsNumber].name, name);
+        strcpy(contacts[*contactsNumber].surname, surname);
+        strcpy(contacts[*contactsNumber].phone, phone);
+        (*contactsNumber)++;
+        printf("Kontakt zostal dodany.\n");
+    }
+}
+
+void searchBySurname(Contact *contacts, int contactsNumber, char *surname){
+    printf("Znalezione kontakty dla nazwiska '%s':\n", surname);
+    int found = 0;
+    for (int i = 0; i < contactsNumber; i++) {
+        if (strcmp(contacts[i].surname, surname) == 0) {
+            printf("Imie: %s, Nazwisko: %s, Numer telefonu: %s\n",
+                   contacts[i].name, contacts[i].surname, contacts[i].phone);
+            found = 1;
+        }
+    }
+    if (!found) {
+        printf("Nie znaleziono kontaktu dla podanego nazwiska.\n");
+    }
+}
+
+void deleteContact(Contact *contacts, int *contactsNumber, char *surname){
+    int index = -1;
+    for (int i = 0; i < *contactsNumber; i++) {
+        if (strcmp(contacts[i].surname, surname) == 0) {
+            index = i;
+            break;
+        }
+    }
+    if (index != -1)    {
+        for (int i = index; i < *contactsNumber - 1; i++){
+            contacts[i] = contacts[i + 1];
+        }
+        (*contactsNumber)--;
+        printf("Kontakt zostal usuniety.\n");
+    } else {
+        printf("Nie znaleziono kontaktu.\n");
+    }
+}
+
+void showAll(Contact *contacts, int contactsNumber){
+    printf("Wszystkie kontakty:\n");
+    for (int i = 0; i < contactsNumber; i++){
+        printf("Imie: %s, Nazwisko: %s, Numer telefonu: %s\n",
+               contacts[i].name, contacts[i].surname, contacts[i].phone);
+    }
+}
+
+int main(){
+    Contact contacts[MAX_CONTACTS];
+    int contactsNumber = 0;
+
+    addContact(contacts, &contactsNumber, "Anna", "Mucha", "794561236");
+    addContact(contacts, &contactsNumber, "Marcin", "Pies", "794852236");
+    addContact(contacts, &contactsNumber, "Jakub", "Bober", "963561236");
+
+    int option;
+    char name[50], surname[50], phone[20];
+
+    do {
+        printf("\nWybierz opcje:\n");
+        printf("1. Dodaj kontakt\n");
+        printf("2. Znajdz kontakt\n");
+        printf("3. Usun kontakt\n");
+        printf("4. Wyswietl wszystkie kontakty\n");
+        printf("0. Zakoncz program\n");
+        scanf("%d", &option);
+
+        switch (option) {
+            case 1:
+                printf("Podaj imie: ");
+                scanf("%s", name);
+                printf("Podaj nazwisko: ");
+                scanf("%s", surname);
+                printf("Podaj numer telefonu: ");
+                scanf("%s", phone);
+                addContact(contacts, &contactsNumber, name, surname, phone);
+                break;
+            case 2:
+                printf("Podaj nazwisko do wyszukania: ");
+                scanf("%s", surname);
+                searchBySurname(contacts, contactsNumber, surname);
+                break;
+            case 3:
+                printf("Podaj nazwisko do usuniecia: ");
+                scanf("%s", surname);
+                deleteContact(contacts, &contactsNumber, surname);
+                break;
+            case 4:
+                showAll(contacts, contactsNumber);
+                break;
+            case 0:
+                printf("Program zakonczony.\n");
+                break;
+            default:
+                printf("Niepoprawna opcja. Wybierz ponownie.\n");
+        }
+    } while (option != 0);
+
+    return 0;
+}
